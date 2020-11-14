c7773da535ea5605770f380d1bfd092afeb679e7
Judhistir <70930235+judhistir88@users.noreply.github.com>
2020-11-14 22:56:14 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -124,11 +124,12 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
         percentage = math.floor(elapsed_time * 100 / total_time)
         progress_str = "📊 <b>Progress:</b> {0}%\n[{1}{2}]".format(
             round(percentage, 2),
-            ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
-            ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))])
+            ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),
+            ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
+            round(percentage, 2))
             )
-        stats = f'📦️ <b>Compressing</b> {target_percentage}%\n\n' \
-                f'⏰️ <b>ETA:</b> {ETA}\n\n' \
+        stats = f'🗜 <b>Compressing</b> {target_percentage}%\n' \
+                f'⏳ <b>ETA:</b> {ETA}\n' \
                 f'{progress_str}\n'
         try:
           await message.edit_text(
@@ -185,7 +186,7 @@ async def take_screen_shot(video_file, output_directory, ttl):
         output_directory,
         str(time.time()) + ".jpg"
     )
-    if video_file.upper().endswith(("MKV", "MP4", "WEBM")):
+    if video_file.upper().endswith(("MKV", "MP4", "WEBM", "AVI" )):
         file_genertor_command = [
             "ffmpeg",
             "-ss",
